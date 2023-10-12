43efb4f10dc9b2879ba1c736aca96b6c68a6cc3c
rieliss <133730089+rieliss@users.noreply.github.com>
2023-10-12 11:55:17 +0700

    1.0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import Nav from "@/components/Nav";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/toggle-mode";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Love you na ttuunn 🌼💕</h1>
+    <main className="p-24 ">
+      <Nav />
+      <section className="py-12 flex flex-col items-center text-center gap-8">
+        <h1 className="text-4xl font-bold">Love you na ttuunn 🌼💕</h1>
+        <p className="text-2xl text-primary">
+          All of your contact information and certificate or everything.
+        </p>
+        <div className="flex gap-6 py-6"></div>
+      </section>
+      <div className="flex gap-6 py-6 items-center justify-center">
+        <Button variant={"secondary"}>Learn more</Button>
+        <Button>Enroll Now</Button>
+      </div>
     </main>
   );
 }
