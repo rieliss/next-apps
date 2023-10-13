9c0cc0bee1259682cf5f946598cd0d31f30bebd2
rieliss <133730089+rieliss@users.noreply.github.com>
2023-10-13 11:57:47 +0700

    1.0.4

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { ModeToggle } from "./ui/toggle-mode";
-import { SparklesIcon } from "@heroicons/react/24/outline";
+import { SparklesIcon } from "@heroicons/react/24/solid";
 
 export default function Nav() {
   return (
@@ -11,7 +11,7 @@ export default function Nav() {
           <li>
             <a href="/" target="_self" rel="noopener noreferrer">
               {" "}
-              <SparklesIcon className="h-8 w-8 text-black" />
+              <SparklesIcon className="h-8 w-8 text-dark:invert" />
             </a>
           </li>
           <li>
