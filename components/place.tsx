c446a59fb4e69e767304dd8654eda935d8dae97c
rieliss <133730089+rieliss@users.noreply.github.com>
2023-10-13 17:46:41 +0700

    1.0.5

--- components/place.tsx
+++ components/place.tsx
@@ -14,7 +14,7 @@ export default function PlacePage({ Img, Name, Description, Link }: Props) {
     <div className="flex items-center justify-center">
       <a
         href={Link}
-        className="flex flex-col bg-gray-100 rounded-lg p-2 h-full items-center group border border-transparent px-4 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        className="flex flex-col rounded-lg p-2 h-full items-center group border border-transparent px-4 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -24,8 +24,10 @@ export default function PlacePage({ Img, Name, Description, Link }: Props) {
           height={0}
           alt="Picture of the author"
         />
-        <h2 className={`mt-3 font-semibold`}>{Name}</h2>
-        <p className={`max-w-[30ch] text-xs opacity-50`}>{Description}</p>
+        <h2 className={`mt-3 font-semibold text-dark:invert`}>{Name}</h2>
+        <p className={`max-w-[30ch] text-xs opacity-50 text-dark:invert`}>
+          {Description}
+        </p>
       </a>
     </div>
   );
