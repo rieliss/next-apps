f6259534466334fe74bc41f64a224601340bba54
rieliss <133730089+rieliss@users.noreply.github.com>
2023-10-18 07:43:57 +0700

    1.0.10

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -81,8 +81,7 @@ const TicketsPage = () => {
       <section className="py-2 flex flex-col items-center text-center gap-8">
         <div className="grid text-center p-2 grid-cols-2 lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
-            onClick={DownTick1}
-            href="/tickets"
+            href="/2.pdf"
             className="group rounded-lg border border-transparent px-5 py-12 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_self"
             rel="noopener noreferrer"
